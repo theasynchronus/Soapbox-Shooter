3cd1e2c2de8da661dc6a6421e00afa3d3cbec7bb
stxllaric.astxrism1603 <omawthankar16032007@gmail.com>
2021-11-23 09:20:54 +0530

    Update main.py
    Making the player visible

--- main.py
+++ main.py
@@ -12,16 +12,23 @@ pygame.display.set_caption("Schrooter")
 
 x = 200
 y = 200
-img = pygame.image.load('.img/player/Idle/0')
-
+img = pygame.image.load('img/player/Idle/0.png')
+rect = img.get_rect()
+rect.center = (x, y)
 run = True
 while run:
+
+
+
+    screen.blit(img, rect)
 
     for event in pygame.event.get():
         #quit event 
         if event.type == pygame.QUIT:
             run = False
 
+
+    pygame.display.update()
 
 pygame.quit            
 
