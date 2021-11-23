4bf8a845551aad4af031c0eebd1ef578215e75dc
stxllaric.astxrism1603 <omawthankar16032007@gmail.com>
2021-11-23 09:32:42 +0530

    Update main.py
    Assigning sprite function

--- main.py
+++ main.py
@@ -10,11 +10,18 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Schrooter")
 
 
+class Soldier(pygame.sprite.Sprite):
+    def __init__(self, x, y, scale):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('img/player/Idle/0.png')
+        img = pygame.transform.scale(
+            img, (int(img.get_width()*scale), (int(img.get_height()*scale))))
+        rect = img.get_rect()
+        rect.center = (x, y)
 x = 200
 y = 200
-img = pygame.image.load('img/player/Idle/0.png')
-rect = img.get_rect()
-rect.center = (x, y)
+scale = 3
+
 run = True
 while run:
 
